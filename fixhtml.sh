779a39b14d66ab5a6870a22d2aec0ac69c8e7455
inponomarev <iponomarev@mail.ru>
2020-10-16 19:05:52 +0300

    fixhtml

---
+++ fixhtml.sh
@@ -0,0 +1,1 @@
+sed -i 'H;${x;s/^\n//;s/dependencies: \[/&\n{ src: \x27http:\/\/remote.jugspeakers.online:3001\/socket.io\/socket.io.js\x27, async: true }\n{ src: \x27http:\/\/remote.jugspeakers.online:3001\/remote.js\x27, async: true }\n/;p;}' slides/target/generated-slides/index.html
